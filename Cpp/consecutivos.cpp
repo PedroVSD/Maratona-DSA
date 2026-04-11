4b6bd47852d810bee8675a775bf0ebdaff65f925
Pedro Vitor <Pedrovsd00@gmail.com>
2026-04-11 19:54:19 -0300

    Consecutivos.cpp

--- Cpp/consecutivos.cpp
+++ Cpp/consecutivos.cpp
@@ -1,31 +1,27 @@
 #include <iostream>
-#include <vector>
-#include <algorithm>
 
 using namespace std;
 
 int main(void){
-
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-
-    int n, pontos = 1, max_pontos = 1;
-
+    int n, ponto = 1, maior = 1;
     cin >> n;
-    vector<int> valores(n);
 
+    int vetor[n];
 
-    for(int i = 0; i < n; i++){
-        cin >> valores[i];
+    for(auto &i: vetor){
+        cin >> i;
     }
 
     for(int i = 1; i < n; i++){
-        if(valores[i] == valores[i-1]){
-            pontos++;
-        } else {
-            max_pontos = max(pontos, max_pontos);
-            pontos = 1;
+        if(vetor[i] == vetor[i-1]){
+            ponto++;
+        }else{
+            ponto = 1;
+        }
+        if(ponto > maior){
+            maior = ponto;
         }
     }
-    cout << max_pontos<< endl;
+
+    cout << maior << endl;
 }
